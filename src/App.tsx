868c6e4759c3f227a6c0ba6345ce46fa33634ce0
IgorBosin <ibosin@yandex.ru>
2023-06-07 02:19:28 +0700

    counter with one display. LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Display} from "./Components/Display/Display";
 import Settings from "./Components/Settings/Settings";
 import s from './App.module.css'
 
 const App = () => {
 
+    // const [counter, setCounter] = useState(JSON.parse(localStorage.getItem('currentValue') || '0'))
     const [counter, setCounter] = useState(0)
     const [startValue, setStartValue] = useState(0)
     const [maxValue, setMaxValue] = useState(5)
     const [setting, setSetting] = useState(true)
 
-
     const settings = () => {
         setSetting(!setting)
         setCounter(startValue)
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
     }
+
+    useEffect(() => {
+        let startValueAsString = localStorage.getItem('startValue')
+        if (startValueAsString) {
+            let newValue = JSON.parse(startValueAsString)
+            setStartValue(newValue)
+        }
+        let maxValueAsString = localStorage.getItem('maxValue')
+        if (maxValueAsString) {
+            let newValue = JSON.parse(maxValueAsString)
+            setMaxValue(newValue)
+        }
+        let currentValueAsString = localStorage.getItem('currentValue')
+        if (currentValueAsString) {
+            let newValue = JSON.parse(currentValueAsString)
+            setCounter(newValue)
+        }
+    }, [])
 
     return (
         <div className={s.border}>
--- src/Components/Display/Display.tsx
+++ src/Components/Display/Display.tsx
@@ -3,30 +3,32 @@ import s from "./Display.module.css";
 import Button from "../Button/Button";
 
 type DisplayType = {
-    counter:number
-    setCounter:(counter:number)=>void
-    startValue:number
-    maxValue:number
-    settings:()=>void
+    counter: number
+    setCounter: (counter: number) => void
+    startValue: number
+    maxValue: number
+    settings: () => void
 }
 
 export const Display: React.FC<DisplayType> = (props) => {
 
-    const {counter, startValue,maxValue,setCounter,settings } = props
+    const {counter, startValue, maxValue, setCounter, settings} = props
 
     const increase = () => {
-        (counter < maxValue) && setCounter(counter + 1)
+        if (counter < maxValue) {
+            setCounter(counter + 1)
+            localStorage.setItem('currentValue', JSON.stringify(counter + 1))
+        }
     }
-
-
 
     const reset = () => {
         setCounter(startValue)
+        localStorage.setItem('currentValue', JSON.stringify(startValue))
     }
 
     return (
         <div className={s.border}>
-            <span className={ `${s.counter} ${counter === maxValue && s.redCounter}`}>{counter}</span>
+            <span className={`${s.counter} ${counter === maxValue && s.redCounter}`}>{counter}</span>
             <div className={s.buttons}>
                 <Button disabled={maxValue === counter} callback={increase} name='inc'/>
                 <Button disabled={startValue === counter} callback={reset} name='reset'/>
